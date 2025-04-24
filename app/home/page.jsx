be02d89b9dae03abafd29176a199c7710ee8cc47
Luiza <luizaschettinii@gmail.com>
2025-04-24 11:38:54 -0300

    refactor: error

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -1,82 +1,130 @@
-"use client"; 
+"use client";
 
-import { useEffect, useState } from "react";
-import axios from "axios"; 
-import CharacterCard from "../../components/CharacterCard";
 import styles from "./Home.module.css";
-import { ToastContainer, toast } from "react-toastify"; 
-import "react-toastify/dist/ReactToastify.css"; 
+import axios from "axios";
+import { useEffect, useState, useRef } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import CharacterCard from "../../components/CharacterCard";
 import Loader from "../../components/Loader";
 
 export default function Home() {
-  const [search, setSearch] = useState(""); 
-  const [characters, setCharacters] = useState([]); 
-  const [notFound, setNotFound] = useState(false); 
-  const [loading, setLoading] = useState(true);
-  const [page, setPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(1);
-
-  const fetchCharacters = async (name, pageNumber) => {
-    setLoading(true);
-
-    try {
-      const { data } = await axios.get(`https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${name}`);
-      setCharacters(data.results);
-      setTotalPages(data.info.pages);
-      setNotFound(false);
-  } catch {
-      setCharacters([]);
-      setNotFound(true);
-  } finally {
-      setLoading(false);
-  }
-
-
-    try {
-      const { data } = await axios.get(
-        `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${name}`
-      ); 
-      setCharacters(data.results); 
-      setTotalPages(data.info.pages);
-      setNotFound(false);
-    } catch {
-      setCharacters([]); 
-      setNotFound(true);
-    }
-  };
-
-  useEffect(() => {
-    fetchCharacters(search.trim(), page); 
-  }, [page]);
   
-  useEffect(() => {
-    fetchCharacters(search, page); 
-  }, [search]);
+    const [characters, setCharacters] = useState([]);
+    const [notFound, setNotFound] = useState(false);
 
-  const handleSearch = () => {
-    const name = search.trim();
-    setPage(1);
-    fetchCharacters(name, 1);
-  };
+    const [loading, setLoading] = useState(true);
 
-  const handleReset = () => {
-    setSearch("");
-    setPage(1);
-    fetchCharacters("", 1);
-    toast.success("Filtro foi resetado", { position: "top-left" });
-  };
+    const cacheRef = useRef(new Map());
 
-  const handleCardClick = (name) => {
-    toast.info(`Você clicou em ${name}`);
-  };
+    const fetchCharacters = async (name = "", pageNumber = 1) => {
+        setLoading(true);
+        const cache = cacheRef.current;
+        const cacheKey = `${name}_${pageNumber}`;
+        const nextPageNumber = pageNumber + 1;
+        const nextCacheKey = `${name}_${nextPageNumber}`;
+
+        const cleanCacheIfNeeded = () => {
+            while (cache.size >= 5) {
+                const firstKey = cache.keys().next().value;
+                cache.delete(firstKey);
+                console.log(`❌ Removido do cache: ${firstKey}`);
+            }
+        };
+
+        console.log("\n============== Iniciar Busca ==============");
+        console.log(`📊 Cache anterior: ${cache.size} páginas`);
+
+        let total = totalPages;
+
+        if (cache.has(cacheKey)) {
+            const cached = cache.get(cacheKey);
+            setCharacters(cached.results);
+            setTotalPages(cached.totalPages);
+            total = cached.totalPages;
+            setNotFound(false);
+            setLoading(false);
+            console.log(`✅ Usando cache: ${cacheKey}`);
+
+        } else {
+            try {
+                const { data } = await axios.get(`https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${name}`);
+
+                cleanCacheIfNeeded();
+                cache.set(cacheKey, {
+                    results: data.results,
+                    totalPages: data.info.pages,
+                }); 
+
+                setCharacters(data.results);
+                setTotalPages(data.info.pages);
+                total = data.info.pages;
+                setNotFound(false);
+                console.log(`💾 Salvo no cache: ${cacheKey}`);
+            } catch {
+                setCharacters([]);
+                setNotFound(true);
+            } finally {
+                setLoading(false);
+            }
+        }
+
+
+        if (nextPageNumber <= total && !cache.has(nextCacheKey)) {
+            try {
+                const res = await axios.get(`https://rickandmortyapi.com/api/character/?page=${nextPageNumber}&name=${name}`);
+                cleanCacheIfNeeded();
+                cache.set(nextCacheKey, {
+                    results: res.data.results,
+                    totalPages: res.data.info.pages,
+                });
+                console.log(`💯 Prefetch salvo: ${nextCacheKey}`);
+            } catch (err) {
+                console.log(`😢 Prefetch falhou: ${nextCacheKey}`, err);
+            }
+        } else {
+            console.log("Prefetch ignorado: já no cache ou fora do limite❗❗❗");
+        }
+
+        console.log(`📦 Cache final: ${cache.size} páginas`);
+        for (const [key, val] of cache.entries()) {
+            console.log(`📦 ${key}: ${val.results.length} personagens`);
+        }
+        console.log("============== Finish ==============\n");
+    };
+
+    useEffect(() => {
+        fetchCharacters();
+    }, []);
+
+    const [search, setSearch] = useState("");
+
+    const handleSearch = () => {
+        setPage(1);
+        fetchCharacters(search, 1);
+    };
+
+    const handleReset = () => {
+        setSearch("");
+        setPage(1);
+        fetchCharacters("", 1);
+        toast.success("Filtro foi resetado", { position: "top-left" });
+    };
+
+    const handleCardClick = (char) => {
+        toast.info(`Você clicou em ${char.name} que está ${char.status}`);
+    };
+
+    const [page, setPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(1);
+
+    useEffect(() => {
+        fetchCharacters(search, page);
+    }, [page]);
 
   return (
     <div className={styles.container}>
-      <ToastContainer
-        position="top-right" 
-        autoClose={7500} 
-        theme="light" 
-      />
+      <ToastContainer position="top-right" autoClose={7500} theme="light" />
 
       <h1 className={styles.title}>Personagens de Rick and Morty</h1>
 
@@ -88,8 +136,7 @@ export default function Home() {
           onChange={(e) => setSearch(e.target.value)}
           className={styles.input}
         />
-        <button
-          onClick={handleSearch} className={styles.buttonSearch}>
+        <button onClick={handleSearch} className={styles.buttonSearch}>
           Buscar
         </button>
         <button onClick={handleReset} className={styles.buttonReset}>
@@ -97,36 +144,47 @@ export default function Home() {
         </button>
       </div>
 
-      {/* Navegação de páginas */}
       <div className={styles.navControls}>
-                <button onClick={() => setPage((p) => Math.max(p - 1, 1))} disabled={page === 1 || notFound} className={styles.buttonNav}>
-                    Página Anterior
-                </button>
-
-                {/* Indicador de página */}
-                <span className={styles.pageIndicator}>
-                    Página {page} de {totalPages}
-                </span>
-
-                <button onClick={() => setPage((p) => Math.min(p + 1, totalPages))} disabled={page === totalPages || notFound} className={styles.buttonNav}>
-                    Próxima Página
-                </button>
-            </div>
-                {notFound && (
-                <h1 className={styles.notFound}>Nenhum personagem encontrado 😢</h1>
-            )}
-            
-
-  
-      <div className={styles.grid}>
-        {characters.map((char) => (
-          <CharacterCard
-            key={char.id}
-            character={char}
-            onClick={() => handleCardClick(char.name)}
-          />
-        ))}
+        <button
+          onClick={() => setPage((p) => Math.max(p - 1, 1))}
+          disabled={page === 1}
+          className={styles.buttonNav}
+        >
+          Página anterior
+        </button>
+        <span className={styles.pageIndicator}>
+          Página {page} de {totalPages}
+        </span>
+        <button
+          onClick={() => setPage((p) => Math.min(p + 1, totalPages))}
+          disabled={page === totalPages || notFound}
+          className={styles.buttonNav}
+        >
+          Próxima Página
+        </button>
       </div>
+
+      {notFound && (
+        <h1 className={styles.notFound}>Nenhum personagem encontrado 😢</h1>
+      )}
+
+      {loading ? (
+        <div
+          className={`${styles.loaderWrapper} ${loading ? "" : styles.hidden}`}
+        >
+          <Loader />
+        </div>
+      ) : (
+        <div className={styles.grid}>
+          {characters.map((char) => (
+            <CharacterCard
+              key={char.id}
+              character={char}
+              onClick={() => handleCardClick(char.name)}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,19 @@
 import "./globals.css";
-			export const metadata = {
-				title: "Rick and Morty App",
-        description: "Meu primeiro consumo de API gratis"
-			};
-			export default function RootLayout({ children }) {
-				return (
-					<html>
-						<body>{children}</body>
-					</html>
-);
-		}
+
+export const metadata = {
+    title: "Rick and Morty App",
+    icons: {
+        icon: "/icon-rick.png", 
+    },
+};
+
+export default function RootLayout({ children }) {
+    return (
+        <html lang="pt-br">
+            <head>
+                <link rel="icon" href="/icon-rick.png" type="image/png" />
+            </head>
+            <body>{children}</body>
+        </html>
+    );
+}
